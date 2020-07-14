cbff56a98b0dbedbaec3b5f99fb78633dce4e791
Mateo <mateo.14.ledesma@gmail.com>
2020-07-13 21:17:10 -0300

    add adsense

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
+          <script data-ad-client="ca-pub-5266488652300571" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
